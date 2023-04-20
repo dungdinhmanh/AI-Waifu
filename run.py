23dc641eeeec10ecd8593ad91e59f06342298244
dungdinhmanh <dungdinhmanh0209@gmail.com>
2023-04-20 03:02:03 +0000

    Updated some files

---
+++ run.py
@@ -0,0 +1,204 @@
+import openai
+import winsound
+import sys
+import pytchat
+import time
+import re
+import pyaudio
+import keyboard
+import wave
+import threading
+import json
+import socket
+from emoji import demojize
+from config import *
+from utils.translate import *
+from utils.TTS import *
+from utils.subtitle import *
+from utils.promptMaker import *
+from utils.twitch_config import *
+
+# to help the CLI write unicode characters to the terminal
+sys.stdout = open(sys.stdout.fileno(), mode='w', encoding='utf8', buffering=1)
+
+# use your own API Key, you can get it from https://openai.com/. I place my API Key in a separate file called config.py
+openai.api_key = api_key
+
+conversation = []
+# Create a dictionary to hold the message data
+history = {"history": conversation}
+
+mode = 0
+total_characters = 0
+chat = ""
+chat_now = ""
+chat_prev = ""
+is_Speaking = False
+owner_name = "Kazuki"
+
+# function to get the user's input audio
+def record_audio():
+    CHUNK = 1024
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1
+    RATE = 44100
+    WAVE_OUTPUT_FILENAME = "input.wav"
+    p = pyaudio.PyAudio()
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+    frames = []
+    print("Recording...")
+    while keyboard.is_pressed('RIGHT_SHIFT'):
+        data = stream.read(CHUNK)
+        frames.append(data)
+    print("Stopped recording.")
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+    wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+    wf.writeframes(b''.join(frames))
+    wf.close()
+    transcribe_audio("input.wav")
+
+# function to transcribe the user's audio
+def transcribe_audio(file):
+    global chat_now
+    try:
+        audio_file= open(file, "rb")
+        # Translating the audio to English
+        # transcript = openai.Audio.translate("whisper-1", audio_file)
+        # Transcribe the audio to detected language
+        transcript = openai.Audio.transcribe("whisper-1", audio_file)
+        chat_now = transcript.text
+        print ("Question: " + chat_now)
+    except Exception as e:
+        print("Error transcribing audio: {0}".format(e))
+        return
+
+    result = owner_name + " said " + chat_now
+    conversation.append({'role': 'user', 'content': result})
+    openai_answer()
+
+# function to get an answer from OpenAI
+def openai_answer():
+    global total_characters, conversation
+
+    total_characters = sum(len(d['content']) for d in conversation)
+
+    while total_characters > 4000:
+        try:
+            # print(total_characters)
+            # print(len(conversation))
+            conversation.pop(2)
+            total_characters = sum(len(d['content']) for d in conversation)
+        except Exception as e:
+            print("Error removing old messages: {0}".format(e))
+
+    with open("conversation.json", "w", encoding="utf-8") as f:
+        # Write the message data to the file in JSON format
+        json.dump(history, f, indent=4)
+
+    prompt = getPrompt()
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=prompt,
+        max_tokens=128,
+        temperature=1,
+        top_p=0.9
+    )
+    message = response['choices'][0]['message']['content']
+    conversation.append({'role': 'assistant', 'content': message})
+
+    translate_text(message)
+
+# translating is optional
+def translate_text(text):
+    global is_Speaking
+    # subtitle will act as subtitle for the viewer
+    # subtitle = translate_google(text, "ID")
+
+    # tts will be the string to be converted to audio
+    detect = detect_google(text)
+    tts = translate_google(text, f"{detect}", "JA")
+    # tts = translate_deeplx(text, f"{detect}", "JA")
+    tts_en = translate_google(text, f"{detect}", "EN")
+    try:
+        # print("ID Answer: " + subtitle)
+        print("JP Answer: " + tts)
+        print("EN Answer: " + tts_en)
+    except Exception as e:
+        print("Error printing text: {0}".format(e))
+        return
+
+    # Choose between the available TTS engines
+    # Japanese TTS
+    # voicevox_tts(tts)
+
+    # Silero TTS, Silero TTS can generate English, Russian, French, Hindi, Spanish, German, etc. Uncomment the line below. Make sure the input is in that language
+    silero_tts(tts_en, "en", "v3_en", "en_21")
+
+    # Generate Subtitle
+    generate_subtitle(chat_now, text)
+
+    time.sleep(1)
+
+    # is_Speaking is used to prevent the assistant speaking more than one audio at a time
+    is_Speaking = True
+    winsound.PlaySound("test.wav", winsound.SND_FILENAME)
+    is_Speaking = False
+
+    # Clear the text files after the assistant has finished speaking
+    time.sleep(1)
+    with open ("output.txt", "w") as f:
+        f.truncate(0)
+    with open ("chat.txt", "w") as f:
+        f.truncate(0)
+
+def preparation():
+    global conversation, chat_now, chat, chat_prev
+    while True:
+        # If the assistant is not speaking, and the chat is not empty, and the chat is not the same as the previous chat
+        # then the assistant will answer the chat
+        chat_now = chat
+        if is_Speaking == False and chat_now != chat_prev:
+            # Saving chat history
+            conversation.append({'role': 'user', 'content': chat_now})
+            chat_prev = chat_now
+            openai_answer()
+        time.sleep(1)
+
+if __name__ == "__main__":
+    try:
+        # You can change the mode to 1 if you want to record audio from your microphone
+        # or change the mode to 2 if you want to capture livechat from youtube
+        mode = input("Mode (1-Mic, 2-Youtube Live, 3-Twitch Live): ")
+
+        if mode == "1":
+            print("Press and Hold Right Shift to record audio")
+            while True:
+                if keyboard.is_pressed('RIGHT_SHIFT'):
+                    record_audio()
+            
+        elif mode == "2":
+            live_id = input("Livestream ID: ")
+            # Threading is used to capture livechat and answer the chat at the same time
+            t = threading.Thread(target=preparation)
+            t.start()
+            yt_livechat(live_id)
+
+        elif mode == "3":
+            # Threading is used to capture livechat and answer the chat at the same time
+            print("To use this mode, make sure to change utils/twitch_config.py to your own config")
+            t = threading.Thread(target=preparation)
+            t.start()
+            twitch_livechat()
+    except KeyboardInterrupt:
+        t.join()
+        print("Stopped")
